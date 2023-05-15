1b4ecb6cdb7510b82eb19c7b2616fe122e219900
Preet Sojitra <preet.dev373@gmail.com>
2023-05-15 11:32:59 +0530

    initial upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Used the following config to allow images from Twitter to be optimized by Next.js
+  images: {
+    domains: ["pbs.twimg.com"],
+  },
 }
 
 module.exports = nextConfig
